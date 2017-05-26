db5b34759d3fcbb06f08ea9db1a5d09df3b59214
Kirill Ishanov <kirill.ishanov@gmail.com>
2017-05-26 11:47:30 -0600

    Update docs to use with latest figwheel and re-frame-datatable

--- docs/project.clj
+++ docs/project.clj
@@ -1,11 +1,11 @@
 (defproject re-frame-datatable-docs "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/clojurescript "1.9.293"]
-                 [figwheel-sidecar "0.5.8"]
+                 [org.clojure/clojurescript "1.9.542"]
+                 [figwheel-sidecar "0.5.10"]
                  [org.clojure/core.async "0.2.395"]
                  [reagent "0.6.0"]
                  [re-frame "0.9.1"]
-                 [re-frame-datatable "0.5.1"]]
+                 [re-frame-datatable "0.5.2-SNAPSHOT"]]
 
   :plugins [[lein-cljsbuild "1.1.4"]]
 
@@ -20,7 +20,7 @@
   :profiles
   {:dev
    {:dependencies [[binaryage/devtools "0.8.2"]]
-    :plugins      [[lein-figwheel "0.5.7"]]}}
+    :plugins      [[lein-figwheel "0.5.10"]]}}
 
   :cljsbuild
   {:builds
