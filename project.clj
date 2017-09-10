68cd4ff5111a69921c7c6c35ef43db86d41f72b9
Kirill Ishanov <kirill.ishanov@gmail.com>
2017-09-09 18:17:05 -0600

    Update core codebase to 0.6.0-snapshot

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject re-frame-datatable "0.5.2"
+(defproject re-frame-datatable "0.6.0-SNAPSHOT"
   :description "DataTable component for re-frame 0.8.0+"
   :url "https://github.com/kishanov/re-frame-datatable"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/clojurescript "1.9.542"]
-                 [reagent "0.6.0"]
-                 [re-frame "0.9.1"]])
+                 [org.clojure/clojurescript "1.9.908"]
+                 [reagent "0.7.0"]
+                 [re-frame "0.10.1"]])
